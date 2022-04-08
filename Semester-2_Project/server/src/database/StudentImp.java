7134b21aa55c5129ccfed81cba5d16b554b33759
Wael Haded <wha@miracle.dk>
2022-04-09 01:37:14 +0200

    add dotenv file

---
+++ Semester-2_Project/server/src/database/StudentImp.java
@@ -0,0 +1,8 @@
+package database;
+import io.github.cdimascio.dotenv.Dotenv;
+public class StudentImp {
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.out.println(dotenv.get("USER_NAME"));
+    }
+}
