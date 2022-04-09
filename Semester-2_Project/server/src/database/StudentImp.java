11c21b5506878c73e22bd369ad622270e26cc8f7
Wael Haded <wha@miracle.dk>
2022-04-09 02:01:21 +0200

    add main folders

--- Semester-2_Project/server/src/database/StudentImp.java
+++ Semester-2_Project/server/src/database/StudentImp.java
@@ -2,8 +2,7 @@ package database;
 import io.github.cdimascio.dotenv.Dotenv;
 public class StudentImp {
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
+     Dotenv dotenv = Dotenv.load();
         System.out.println(dotenv.get("USER_NAME"));
-        System.out.println(dotenv.get("DATABASE_URL"));
     }
 }
