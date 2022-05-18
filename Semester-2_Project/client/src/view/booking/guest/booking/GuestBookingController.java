50f54e583734a33525ddd0f96f266db07fa27e68
DESKTOP-EEIUHDN\Syamend <317546@via.dk>
2022-05-18 14:54:03 +0100

    creating the controller for Teacher and Guest

--- Semester-2_Project/client/src/view/booking/guest/booking/GuestBookingController.java
+++ Semester-2_Project/client/src/view/booking/guest/booking/GuestBookingController.java
@@ -1,15 +1,41 @@
 package view.booking.guest.booking;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import view.ViewHandler;
+import viewModel.booking.GuestBookingViewModel;
 
 public class GuestBookingController {
+    public ListView roomsList;
+    public DatePicker bookingDate;
+    public TextField startTime;
+    public TextField endTime;
     private ViewHandler viewHandler;
+    private GuestBookingViewModel viewModel;
+    private TextField date;
 
-    public void init(ViewHandler viewHandler){
+    public void init(ViewHandler viewHandler, GuestBookingViewModel viewModel){
+        this.viewModel=viewModel;
         this.viewHandler = viewHandler;
+        bookingDate.valueProperty().addListener((observable ,oldValue,newValue)-> {
+            date.textProperty().set(newValue.toString());
+        });
+        viewModel.bindDate(date.textProperty());
+        viewModel.bindStartTime(startTime.textProperty());
+        viewModel.bindEndTime(endTime.textProperty());
     }
     public void logout(){
         viewHandler.getCurrentUser().setCurrentUser(null);
         viewHandler.openLogin();
+    }
+
+    public void getAvailableRooms(ActionEvent actionEvent) {
+        roomsList.setItems(viewModel.getAvailableRooms());
+    }
+
+    public void book(ActionEvent actionEvent) {
+        System.out.println("Booking");
     }
 }
--- Semester-2_Project/client/src/viewModel/booking/GuestBookingViewModel.java
+++ Semester-2_Project/client/src/viewModel/booking/GuestBookingViewModel.java
@@ -1,12 +1,58 @@
 package viewModel.booking;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import model.booking.BookingModel;
+import room_model.Room;
+import room_model.Rooms;
+
+import java.rmi.RemoteException;
+import java.sql.Timestamp;
+import java.util.Date;
 
 public class GuestBookingViewModel {
     private BookingModel bookingModel;
+    private StringProperty date;
+    private StringProperty startTime;
+    private StringProperty endTime;
+    private ObservableList<Room> roomsList;
+    private StringProperty error;
 
     public GuestBookingViewModel(BookingModel bookingModel){
         this.bookingModel = bookingModel;
+        this.date = new SimpleStringProperty("");
+        this.startTime = new SimpleStringProperty("");
+        this.endTime = new SimpleStringProperty("");
+        this.roomsList = FXCollections.observableArrayList();
+        this.error = new SimpleStringProperty("");
     }
 
+    public void bindDate(StringProperty textProperty) {
+        textProperty.bindBidirectional(date);
+    }
+
+    public void bindStartTime(StringProperty textProperty) {
+        textProperty.bindBidirectional(startTime);
+    }
+
+    public void bindEndTime(StringProperty textProperty) {
+        textProperty.bindBidirectional(endTime);
+    }
+
+    public ObservableList getAvailableRooms() {
+        try {
+            Date d = new Date(date.get());
+            Timestamp startDateTime = new Timestamp(d.getYear()-1900,d.getMonth(),d.getDate(),Integer.parseInt(startTime.get()),0,0,0);
+            Rooms rooms =  bookingModel.getAvailableRooms(startDateTime,startDateTime);
+            this.roomsList.clear();
+            this.roomsList.addAll(rooms.getRooms());
+            return roomsList;
+        } catch (RemoteException e) {
+            error.set(e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
